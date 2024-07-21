fa654aa777f57cc1ec1b765d60021f7cc902f5ca
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-07-21 11:17:02 +0600

    send stop list added

--- src/components/Update/HideFood.vue
+++ src/components/Update/HideFood.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <!-- <div>
     <Button
       label="Скрыть еду"
       severity="secondary"
@@ -11,6 +11,7 @@
       modal
       header="Скрыть еду"
       :style="{ width: '35rem' }"
+      
     >
       <div class="card flex gap-3 mb-5 flex-column">
         <Dropdown
@@ -48,24 +49,7 @@
           :selectionLimit="3"
           @change="handleFoodFalse"
         />
-        <!-- <Dropdown v-model="selectedFoodFalse" v-if='isFoodOpen' placeholder="Выберите еду" filter :options="selectedFoods?.Foods" optionLabel="name"
-             @change="handleFoodFalse"
-             
-             >
-                <template #value="slotProps">
-                    <div v-if="slotProps.value" class="flex align-items-center">
-                        <div>{{ slotProps.value.name }}</div>
-                    </div>
-                    <span v-else>
-                        {{ slotProps.placeholder }}
-                    </span>
-                </template>
-                <template #option="slotProps">
-                    <div class="flex align-items-center">
-                        <div>{{ slotProps.option.name }}</div>
-                    </div>
-                </template>
-</Dropdown> -->
+      
 
         <div v-if="showCheck" class="checkbox-hide">
           <Checkbox v-model="checked" :binary="true" id="hide-food" />
@@ -80,19 +64,126 @@
         />
       </div>
     </Dialog>
-  </div>
+  </div> -->
+
+<div>
+<h3>Отправить на СТОП-ЛИСТ</h3>
+
+  <DataTable
+      v-model:expandedRows="expandedRows"
+      v-model:filters="filtersСategory"
+      filterDisplay="row"
+      :value="stoppedListFoodsFalse"
+      dataKey="id"
+    >
+      <template #header>
+        <div class="flex flex-column justify-content-between mb-3">
+          <div class="flex flex-wrap gap-2">
+            <InputText
+              v-model="globalSearch"
+              type="text"
+              @input="handleGlobalSearch"
+              class="w-30rem p-column-filter"
+              placeholder="Поиск"
+            />
+          </div>
+          <div class="flex flex-wrap gap-2">
+            <Button
+              text
+              icon="pi pi-plus"
+              label="Открыть все"
+              @click="expandAll"
+            />
+            <Button
+              text
+              icon="pi pi-minus"
+              label="Скрыть все"
+              @click="collapseAll"
+            />
+          </div>
+        </div>
+      </template>
+      <Column expander style="width: 1rem" />
+      <Column field="name" header="Категория">
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="w-20rem p-column-filter"
+            placeholder="Поиск по категориям"
+          />
+        </template>
+      </Column>
+      <template #expansion="slotProps">
+        <div class="pt-0 pb-3 pl-3 pr-3">
+          <DataTable
+            :value="slotProps?.data?.foods"
+            v-model:filters="filters"
+            filterDisplay="row"
+            style="margin-right: -20px"
+          >
+            <Column
+              field="name"
+              header="Название"
+              class="flex nameSearch"
+            >
+              <template #filter="{ filterModel, filterCallback }">
+                <InputText
+                  v-model="filterModel.value"
+                  type="text"
+                  @input="filterCallback()"
+                  class="w-full p-column-filter"
+                  placeholder="Поиск по имени"
+                />
+              </template>
+
+              <template #body="slotProps">
+                <div class="flex align-items-center gap-5 justify-content-between">
+                  <span>
+                    {{ slotProps.data.name }}
+                  </span>
+                  <Button icon="pi pi-check" severity="info" v-tooltip.top="'Отправить на СТОП-ЛИСТ'" @click="sendToStopList(slotProps.data.name)"></Button>
+
+               
+                </div>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
+      </template>
+    </DataTable>
+
+
+    <Dialog modal header="СТОП-ЛИСТ" v-model:visible="openStopListModal">
+
+      <ConfirmButtons
+      confirmText="Потвердить"
+      declineText="Отменить"
+      :descrText="`Вы действительно хотите отправить на СТОП-ЛИСТ ${selectedItemStopList}`"
+      @confirmAction="confirmSendStopList"
+      @closeModal="openStopListModal = false"
+    />
+
+    </Dialog>
+    <Toast/>
+</div>
 </template>
 
 <script setup lang="ts">
 import http from "@/http";
 import { useToast } from "primevue/usetoast";
+import ConfirmButtons from '@/components/UI/ConfirmButtons.vue'
 import { ref, onMounted } from "vue";
-import { Category, CategoryWithFoods } from "@/types/Category";
+import { Category, CategoryWithFoods ,CategoryWithFoodsUpdated} from "@/types/Category";
 import { useStore } from "vuex";
 import { Food } from "@/types/Food";
 const isModalVisibleFood = ref(false);
 const store = useStore();
 const showCheck = ref(false);
+const globalSearch =ref('')
+import { FilterMatchMode } from "primevue/api";
+const expandedRows = ref();
 const checked = ref(false);
 const selectedCategory = ref();
 const toast = useToast();
@@ -100,8 +191,10 @@ const selectedFoodFalse = ref();
 const selectedFoods = ref({} as CategoryWithFoods);
 const categories = ref([] as Category[]);
 const isFoodOpen = ref(false);
-const stoppedListFoodsFalse = ref([] as CategoryWithFoods[]);
-
+const stoppedListFoodsFalse = ref([] as CategoryWithFoodsUpdated[]);
+const filteredFoods = ref([] as CategoryWithFoodsUpdated[]);
+const openStopListModal=ref(false)
+const selectedItemStopList=ref('')
 const selectCategory = (event: any) => {
   console.log("event", event);
   isFoodOpen.value = true;
@@ -109,14 +202,68 @@ const selectCategory = (event: any) => {
     selectedFoodFalse.value = null;
     showCheck.value = false;
   }
-  const filteredFoods = stoppedListFoodsFalse?.value?.find(
-    (item) => item?.Category?.id === event?.value?.id,
-  );
-  if (filteredFoods !== undefined && filteredFoods) {
-    selectedFoods.value = filteredFoods;
+};
+
+const confirmSendStopList =async()=>{
+    const status = await store.dispatch("sendFoodToStopList", {
+    foodName: selectedItemStopList?.value,
+    status: true,
+  });
+  if (status === 200) {
+    toast.add({
+      severity: "success",
+      detail: "Видимость еды обновлена!",
+      summary: "Успешно",
+    });
+    openStopListModal.value= false;
+    store.dispatch("getAllCategoryNames");
+    setTimeout(()=>{
+      window.location.reload()
+    },500)
+  }
+
+}
+const sendToStopList = (foodName:string)=>{
+  openStopListModal.value = true
+  selectedItemStopList.value=foodName
+
+}
+
+
+const handleGlobalSearch = () => {
+  if (globalSearch && globalSearch?.value?.length > 0) {
+    const value = globalSearch?.value?.toLocaleLowerCase();
+
+   stoppedListFoodsFalse.value = filteredFoods.value
+        ?.map((item) => {
+          const filteredFoods = item.foods.filter((foodItem) => {
+            const foodName = foodItem?.name?.toLowerCase();
+            if (
+              item?.name?.toLowerCase()?.includes(value) ||
+              foodName?.includes(value.toLowerCase())
+            ) {
+              return true;
+            }
+            return false;
+          });
+
+          return {
+            name: item.name,
+            id: item.id,
+            foods: filteredFoods,
+          };
+        })
+        .filter((category) => category.foods.length > 0);
   }
 };
 
+
+const filters = ref({
+  name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const filtersСategory = ref({
+  name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 const handleFoodFalse = (event: any) => {
   showCheck.value = true;
 };
@@ -166,8 +313,15 @@ const getStoppedFoods = async () => {
   try {
     const response = await http("admin/get-all-foods-stoplist-false");
     if (response.status === 200) {
-      stoppedListFoodsFalse.value = response.data;
-      console.log("stopped list false response", response.data);
+      stoppedListFoodsFalse.value = response.data?.map(
+            (item: CategoryWithFoods) => ({
+              id: item.Category.id,
+              name: item.Category.name,
+              foods: item.Foods,
+            }),
+          );
+          filteredFoods.value=stoppedListFoodsFalse.value
+      console.log("stopped list false response", stoppedListFoodsFalse);
       categories.value = response.data?.map(
         (item: CategoryWithFoods) => item?.Category,
       );
@@ -177,7 +331,16 @@ const getStoppedFoods = async () => {
     console.log(err);
   }
 };
+const expandAll = () => {
+  expandedRows.value = stoppedListFoodsFalse.value?.reduce(
+    (acc: any, p: any) => (acc[p.id] = true) && acc,
+    {},
+  );
+};
 
+const collapseAll = () => {
+  expandedRows.value = null;
+};
 onMounted(async () => {
   await store.dispatch("getAllCategoryNames");
   getStoppedFoods();
--- src/components/Update/ShowFood.vue
+++ src/components/Update/ShowFood.vue
@@ -1,80 +1,114 @@
 <template>
-  <div>
-    <Button
-      label="Показать еду"
-      severity="secondary"
-      rounded
-      @click="isModalVisibleFood = !isModalVisibleFood"
-    />
-    <Dialog
-      v-model:visible="isModalVisibleFood"
-      modal
-      header="Показать еду"
-      :style="{ width: '35rem' }"
-    >
-      <div class="card flex gap-3 mb-5 flex-column">
-        <Dropdown
-          v-model="selectedCategory"
-          :options="categories"
-          filter
-          optionLabel="name"
-          placeholder="Выберите категорию"
-          @change="selectCategory"
-        >
-          <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex align-items-center">
-              <div>{{ slotProps.value.name }}</div>
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-          <template #option="slotProps">
-            <div class="flex align-items-center">
-              <div>{{ slotProps.option.name }}</div>
-            </div>
-          </template>
-        </Dropdown>
+ 
 
-        <MultiSelect
-          display="chip"
-          v-model="selectedFoodFalse"
-          v-if="isFoodOpen"
-          :options="selectedFoods?.Foods"
-          filter
-          optionLabel="name"
-          placeholder="Выберите еду"
-          :maxSelectedLabels="3"
-          :selectionLimit="3"
-          @change="handleFoodFalse"
-        />
-
-        <div v-if="showCheck" class="checkbox-hide">
-          <Checkbox v-model="checked" :binary="true" id="hide-food" />
-          <label for="hide-food" class="ml-1"> Показать </label>
-        </div>
-        <Button
-          v-if="showCheck"
-          label="Изменить"
-          severity="info"
-          class="mt-5"
-          @click="hideFood"
+<div>
+  <h3>Снять с СТОП-ЛИСТА</h3>
+  <DataTable
+  v-model:expandedRows="expandedRows"
+  v-model:filters="filtersСategory"
+  filterDisplay="row"
+  :value="stoppedListFoodsFalse"
+  dataKey="id"
+>
+  <template #header>
+    <div class="flex flex-column justify-content-between mb-3">
+      <div class="flex flex-wrap gap-2">
+        <InputText
+          v-model="globalSearch"
+          type="text"
+        @input="handleGlobalSearch"
+          class="w-30rem p-column-filter"
+          placeholder="Поиск"
         />
       </div>
-    </Dialog>
-  </div>
+      <div class="flex flex-wrap gap-2">
+        <Button
+          text
+          icon="pi pi-plus"
+          label="Открыть все"
+          @click="expandAll"
+        />
+        <Button
+          text
+          icon="pi pi-minus"
+          label="Скрыть все"
+          @click="collapseAll"
+        />
+      </div>
+    </div>
+  </template>
+  <Column expander style="width: 1rem" />
+  <Column field="name" header="Категория">
+    <template #body="slotProps">
+<span >{{ slotProps.data.name }}</span>
+    </template>
+    <template #filter="{ filterModel, filterCallback }">
+      <InputText
+        v-model="filterModel.value"
+        type="text"
+        @input="filterCallback()"
+        class="w-20rem p-column-filter"
+        placeholder="Поиск по категориям"
+      />
+    </template>
+  </Column>
+  <template #expansion="slotProps">
+    <div class="pt-0 pb-3 pl-3 pr-3">
+      <DataTable
+        :value="slotProps?.data?.foods"
+        v-model:filters="filters"
+        filterDisplay="row"
+        style="margin-right: -20px"
+      >
+        <Column
+          field="name"
+          header="Название"
+          class="flex nameSearch"
+        >
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              type="text"
+              @input="filterCallback()"
+              class="w-full p-column-filter"
+              placeholder="Поиск по имени"
+            />
+          </template>
+
+          <template #body="slotProps">
+            <div class="flex align-items-center gap-5">
+              {{ slotProps.data.name }}
+              <Button icon="pi pi-times" severity="danger" v-tooltip.top="'Снять с СТОП-ЛИСТА'"></Button>
+
+           
+            </div>
+          </template>
+        </Column>
+
+         
+
+       
+        <template #empty> Нет данных. </template>
+      </DataTable>
+    </div>
+  </template>
+</DataTable>
+</div>
 </template>
 
 <script setup lang="ts">
 import http from "@/http";
 import { useToast } from "primevue/usetoast";
 import { ref, onMounted } from "vue";
-import { Category, CategoryWithFoods } from "@/types/Category";
+import { Category, CategoryWithFoods,CategoryWithFoodsUpdated } from "@/types/Category";
 import { useStore } from "vuex";
 import { Food } from "@/types/Food";
 const isModalVisibleFood = ref(false);
 const store = useStore();
 const showCheck = ref(false);
+const globalSearch =ref('')
+import { FilterMatchMode } from "primevue/api";
+const expandedRows = ref();
 const checked = ref(false);
 const selectedCategory = ref();
 const toast = useToast();
@@ -82,7 +116,8 @@ const selectedFoodFalse = ref();
 const selectedFoods = ref({} as CategoryWithFoods);
 const categories = ref([] as Category[]);
 const isFoodOpen = ref(false);
-const stoppedListFoodsFalse = ref([] as CategoryWithFoods[]);
+const stoppedListFoodsFalse = ref([] as CategoryWithFoodsUpdated[]);
+const filteredFoods = ref([] as CategoryWithFoodsUpdated[]);
 
 const selectCategory = (event: any) => {
   console.log("event", event);
@@ -91,20 +126,65 @@ const selectCategory = (event: any) => {
     selectedFoodFalse.value = null;
     showCheck.value = false;
   }
-  const filteredFoods = stoppedListFoodsFalse?.value?.find(
-    (item) => item?.Category?.id === event?.value?.id,
-  );
-  if (filteredFoods !== undefined && filteredFoods) {
-    selectedFoods.value = filteredFoods;
-  }
+  // const filteredFoods = stoppedListFoodsFalse?.value?.find(
+  //   (item) => item?.Category?.id === event?.value?.id,
+  // );
+  // if (filteredFoods !== undefined && filteredFoods) {
+  //   selectedFoods.value = filteredFoods;
+  // }
 };
+
+const filters = ref({
+  name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const filtersСategory = ref({
+  name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 
 const handleFoodFalse = (event: any) => {
   showCheck.value = true;
 };
 
+const expandAll = () => {
+  expandedRows.value = stoppedListFoodsFalse.value?.reduce(
+    (acc: any, p: any) => (acc[p.id] = true) && acc,
+    {},
+  );
+};
+
+const handleGlobalSearch = () => {
+  if (globalSearch && globalSearch?.value?.length > 0) {
+    const value = globalSearch?.value?.toLocaleLowerCase();
+   stoppedListFoodsFalse.value = filteredFoods.value
+        ?.map((item) => {
+          const filteredFoods = item.foods.filter((foodItem) => {
+            const foodName = foodItem?.name?.toLowerCase();
+            if (
+              item?.name?.toLowerCase()?.includes(value) ||
+              foodName?.includes(value.toLowerCase())
+            ) {
+              return true;
+            }
+            return false;
+          });
+
+          return {
+            name: item.name,
+            id: item.id,
+            foods: filteredFoods,
+          };
+        })
+        .filter((category) => category.foods.length > 0);
+  }
+};
+
+const collapseAll = () => {
+  expandedRows.value = null;
+};
+
 const hideFood = () => {
   const foodNames = selectedFoodFalse?.value?.map((item: Food) => item?.name);
+
   if (foodNames?.length > 0) {
     foodNames.forEach(async (name: string) => {
       const body = {
@@ -146,8 +226,17 @@ const getStoppedFoods = async () => {
   try {
     const response = await http("admin/get-all-foods-stoplist-true");
     if (response.status === 200) {
-      stoppedListFoodsFalse.value = response.data;
-      console.log("stopped list false response", response.data);
+     const result = response.data?.map(
+            (item: CategoryWithFoods) => ({
+              id: item.Category.id,
+              name: item.Category.name,
+              foods: item.Foods,
+            }),
+          );
+
+          stoppedListFoodsFalse.value = result?.filter((item:CategoryWithFoodsUpdated)=>item?.foods!==null)
+          filteredFoods.value=stoppedListFoodsFalse.value
+      console.log("stoppedListFoodsFalse", stoppedListFoodsFalse.value);
       categories.value = response.data?.map(
         (item: CategoryWithFoods) => item?.Category,
       );
